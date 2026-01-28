45efe601c3ad81c51fc560a741256ebc39283b54
Violet <z5476543@ad.unsw.edu.au>
2026-01-28 13:48:06 +1100

    north south shadow length and module height diff

--- shading/NorthSouth.py
+++ shading/NorthSouth.py
@@ -4,21 +4,40 @@ from __future__ import annotations
 import math
 from dataclasses import dataclass
 
+from ..ProjectConstraints import ProjectConstraints, ShadingConstraints
+
 
 @dataclass
 class NorthSouth:
-    sun_angle: float  # degrees
-    azimuth: float  # degrees
-    min_gap_btwn_end_modules: float
+    constraints: ShadingConstraints
+
+    def __init__(self, constraints: ProjectConstraints) -> None:
+        if not constraints.with_shading:
+            raise ValueError("NorthSouth shading analysis requires with_shading=True.")
+        assert isinstance(constraints, ShadingConstraints)
+        self.constraints = constraints
+
+    @property
+    def azimuth(self) -> float:
+        return self.constraints.azimuth_deg
+
+    @property
+    def pitch(self) -> float:
+        return self.constraints.pitch
+
+    @property
+    def sun_angle(self) -> float:
+        return self.constraints.sun_angle
+
+    @property
+    def min_gap_btwn_end_modules(self) -> float:
+        return self.constraints.min_gap_btwn_end_modules
+
+    def ns_shadow_length(self) -> float:
+        max_shadow_length = 1000 / (math.tan(self.sun_angle))
+        return math.sin((90 - self.azimuth) * (math.pi / 180)) * (max_shadow_length / 1000)
 
     def max_height_diff(self) -> float:
-        max_shadow_length = 1000 / (math.tan(self.sun_angle))
-        ns_shadow_length = math.sin((90 - self.azimuth) * (math.pi / 180)) * (
-            max_shadow_length / 1000
-        )
+        ns_shadow_length = self.ns_shadow_length()
         max_module_height_diff = self.min_gap_btwn_end_modules / ns_shadow_length
         return max_module_height_diff
-
-        """
-        run before grading 
-        """
